537b89651f9c3a709950c19f2b452fa4e92e10fa
NicolasFrance01 <francenicolas.dw@gmail.com>
2026-06-27 19:49:05 -0300

    Fix typescript type error in prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@prisma/config'
+import 'dotenv/config'
 
 export default defineConfig({
-  earlyAccess: true,
   migrations: {
     seed: 'npx tsx prisma/seed.ts',
   },
